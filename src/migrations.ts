d60249178e4eb217e45282cd25b826e0ebd9a53e
Badr Chanaa <badrchanaa@gmail.com>
2026-01-09 15:17:03 +0100

    feat: foreign keys

--- src/migrations.ts
+++ src/migrations.ts
@@ -16,6 +16,19 @@ export interface Field {
   constraints?: Constraint;
 }
 
+export type ReferentialAction = "CASCADE" | "SET NULL" | "RESTRICT";
+
+export type ForeignKeyOptions = { deferred?: boolean } & (
+  | {
+      onDelete: ReferentialAction;
+      onUpdate?: ReferentialAction;
+    }
+  | {
+      onDelete?: ReferentialAction;
+      onUpdate: ReferentialAction;
+    }
+);
+
 /**
  * Drop a database table
  *
@@ -26,11 +39,119 @@ export function DropTable(tableName: string) {
   return QueryGenerator.dropTable(tableName);
 }
 
+export class ForeignKey {
+  constructor(
+    private readonly refTable: string,
+    private readonly references: Record<string, string>,
+    private readonly onUpdate?: ReferentialAction,
+    private readonly onDelete?: ReferentialAction,
+    private readonly deferred: boolean = false,
+  ) {}
+
+  SQL(): string {
+    const entries = Object.entries(this.references);
+    const childKey = entries.map(([col]) => `"${col}"`).join(", ");
+    const parentKey = entries.map(([, refCol]) => `"${refCol}"`).join(", ");
+    const parts = [
+      `FOREIGN KEY(${childKey}) REFERENCES ${this.refTable}(${parentKey})`,
+    ];
+    if (this.onUpdate) parts.push(`ON UPDATE ${this.onUpdate}`);
+    if (this.onDelete) parts.push(`ON DELETE ${this.onDelete}`);
+    if (this.deferred) parts.push("DEFERRABLE INITIALLY DEFERRED");
+    return parts.join(" ");
+  }
+}
+
 export class Table {
-  private fieldNames: Set<string>;
-  private _fields: Field[];
-  public name: string;
+  private fieldsMap: Map<string, Field> = new Map();
   private hasPk: boolean = false;
+  public name: string;
+  private _foreignKeys: Map<string, ForeignKey> = new Map();
+
+  constructor(tableName: string) {
+    this.validateTableName(tableName);
+    this.name = tableName.toLowerCase();
+  }
+
+  private get fieldNames() {
+    return this.fieldsMap.keys();
+  }
+
+  addField(name: string, type: FieldType, constraints: Constraint = 0) {
+    name = name.toLowerCase();
+    if (this.fieldsMap.has(name))
+      throw new Error(`table fields can not have the same name ${name}`);
+    const isPk = constraints & Constraint.PRIMARY_KEY;
+    if (isPk && this.hasPk)
+      throw new Error(`table cannot have more than one primary key`);
+    if (isPk) this.hasPk = true;
+    this.fieldsMap.set(name, { name, type, constraints });
+    return this;
+  }
+
+  addForeignKey(
+    refTable: string | Table,
+    references: Readonly<Record<string, string>>,
+    options?: Readonly<ForeignKeyOptions>,
+  ) {
+    const tableName = (
+      refTable instanceof Table ? refTable.name : refTable
+    ).toLowerCase();
+    const sortedEntries = Object.entries(references).sort(([a], [b]) =>
+      a.localeCompare(b, undefined, { sensitivity: "base" }),
+    );
+    // child keys
+    const columns = new Set(sortedEntries.map(([col]) => col.toLowerCase()));
+    // parent keys
+    const refColumns = new Set(
+      sortedEntries.map(([, refCol]) => refCol.toLowerCase()),
+    );
+
+    if (sortedEntries.length === 0)
+      throw new Error("Foreign key must reference at least one column");
+    if (
+      refTable instanceof Table &&
+      refColumns.difference(new Set(refTable.fieldNames)).size !== 0
+    ) {
+      throw new Error(
+        "Foreign key referenced column(s) does not exist on referenced table",
+      );
+    }
+    if (columns.difference(new Set(this.fieldNames)).size !== 0) {
+      throw new Error("Foreign key column(s) does not exist on table");
+    }
+    const fk = new ForeignKey(
+      tableName,
+      references,
+      options?.onUpdate,
+      options?.onDelete,
+      options?.deferred ?? false,
+    );
+    let key = tableName + "\0";
+    for (const [col, refCol] of sortedEntries) {
+      key += `\0${col.toLowerCase()}:\0${refCol.toLowerCase()}`;
+    }
+    if (this._foreignKeys.has(key)) {
+      throw new Error("Foreign key already exists");
+    }
+    this._foreignKeys.set(key, fk);
+  }
+
+  get foreignKeys() {
+    return this._foreignKeys.values();
+  }
+
+  get fields() {
+    return this.fieldsMap.values();
+  }
+
+  create(): string {
+    return QueryGenerator.createTable(this);
+  }
+
+  update(): string {
+    return "";
+  }
 
   private validateTableName(name: string) {
     if (name.length === 0) throw new Error("Invalid empty table name");
@@ -54,37 +175,6 @@ export class Table {
         "Invalid table name: unquoted table name can only contain alphanumeric and underscore characters",
       );
     }
-  }
-
-  constructor(tableName: string) {
-    this.validateTableName(tableName);
-    this.name = tableName;
-    this.fieldNames = new Set();
-    this._fields = new Array();
-  }
-
-  addField(name: string, type: FieldType, constraints: Constraint = 0) {
-    if (this.fieldNames.has(name))
-      throw new Error(`table fields can not have the same name ${name}`);
-    const isPk = constraints & Constraint.PRIMARY_KEY;
-    if (isPk && this.hasPk)
-      throw new Error(`table can not have more than one primary key`);
-    if (isPk) this.hasPk = true;
-    this._fields.push({ name, type, constraints });
-    this.fieldNames.add(name);
-    return this;
-  }
-
-  get fields(): Field[] {
-    return this._fields;
-  }
-
-  create(): string {
-    return QueryGenerator.createTable(this);
-  }
-
-  update(): string {
-    return "";
   }
 }
 
--- src/query-generator.ts
+++ src/query-generator.ts
@@ -1,35 +1,33 @@
-import { Constraint, Table } from "./migrations";
-import type { Field } from "./migrations";
+import { Constraint, Table, type Field } from "./migrations";
 
 export class QueryGenerator {
   private static getFieldConstraints(field: Field) {
     const constraints = field.constraints;
-    let constraintsStr = "";
     if (!constraints) return "";
-    if (constraints & Constraint.UNIQUE) constraintsStr += "UNIQUE ";
-    if (constraints & Constraint.PRIMARY_KEY) constraintsStr += "PRIMARY KEY ";
-    if (constraints & Constraint.NOT_NULL) constraintsStr += "NOT NULL ";
-    return constraintsStr.trim();
+    const parts: string[] = [];
+    if (constraints & Constraint.PRIMARY_KEY) parts.push("PRIMARY KEY");
+    if (constraints & Constraint.NOT_NULL) parts.push("NOT NULL");
+    if (constraints & Constraint.UNIQUE) parts.push("UNIQUE");
+    return parts.join(" ");
   }
 
-  private static generateFieldQuery(field: Field) {
-    // NOTE: Validation is handled by typescript
-    const ftypeStr = field.type === "int" ? "INTEGER" : field.type;
-    return `${field.name} ${ftypeStr} ${this.getFieldConstraints(field)}`;
+  private static generateFieldSQL(field: Field) {
+    const fieldType = field.type === "int" ? "INTEGER" : field.type;
+    const constraints = this.getFieldConstraints(field);
+    if (!constraints) return `${field.name} ${fieldType}`;
+    return `${field.name} ${fieldType} ${constraints}`;
   }
 
   public static createTable(table: Table) {
-    if (table.fields.length === 0)
+    const fields = Array.from(table.fields);
+    if (fields.length === 0)
       throw new Error("can not create a table with no fields");
-
-    const fieldsQuery = table.fields
-      .map((field) => {
-        return this.generateFieldQuery(field);
-      })
-      .join(",\n");
-    const query = `CREATE TABLE IF NOT EXISTS ${table.name}(
-${fieldsQuery}
-);`;
+    const fieldsSQL = fields.map((field) => this.generateFieldSQL(field));
+    if (table.foreignKeys) {
+      const fkSQL = [...table.foreignKeys].map((fk) => fk.SQL());
+      fieldsSQL.push(...fkSQL);
+    }
+    const query = `CREATE TABLE IF NOT EXISTS ${table.name}(${fieldsSQL.join(",\n")});`;
     return query;
   }
 
